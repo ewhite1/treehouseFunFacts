f42eaee30ea91f22378bedf52e16233b68dd5287
Edward White <edwardwhite@outlook.com>
2015-12-10 02:26:01 -0600

    Initial Commit

---
+++ Fun Facts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fun Facts
+//
+//  Created by Edward White on 12/10/15.
+//  Copyright © 2015 Positivecapitalism. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
