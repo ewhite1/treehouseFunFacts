f24ffc99d744c2d23edbd036cc2f9d0dc556fc0d
Edward White <edwardwhite@outlook.com>
2015-12-13 21:47:34 -0600

    added random number generator to the struct inside our fact book model

--- Fun Facts/FactBook.swift
+++ Fun Facts/FactBook.swift
@@ -20,6 +20,14 @@ struct FactBook {
         "Some penguins can leap 2-3 meters out of the water.",
         "On average, it takes 66 days to form a new habit.",
         "Mammoths still walked the earth when the Great Pyramid was being built."
-    
+        
     ]
+    func randomFact() -> String {
+        var unsignedArrayCount = UInt32(factArray.count)
+        var unsignedRandomNumber = arc4random_uniform(unsignedArrayCount)
+        var randomNumber = Int(unsignedRandomNumber)
+        
+        return factArray[randomNumber]
+        
+    }
 }
